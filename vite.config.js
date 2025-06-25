8d80e108cc18e7463ba8f6d92982e36f0c1019f2
FredericFsa <frederic.salerno@gmail.com>
2025-06-25 13:43:58 +0200

    🔧 Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/qr_code_reader/', // ✅ Ajoute cette ligne !
   plugins: [react()],
 });
